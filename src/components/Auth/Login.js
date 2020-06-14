b6f9cdbf6b5f58f1d0e19365aeb2903b0e048d84
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-06-14 15:00:21 -0700

    create reusable useFormVal hook and form controls for login

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,22 +1,55 @@
 import React, { useState } from "react";
+import useFormValidation from "./useFormValidation";
+
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  password: "",
+};
 
 function Login(props) {
+  const { handleChange, handleSubmit, values } = useFormValidation(INITIAL_STATE);
   const [login, setLogin] = useState(true);
+
   return (
     <div>
       <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
-      <form className="flex flex-column">
+      <form className="flex flex-column" onSubmit={handleSubmit}>
         {!login && (
-          <input type="text" placeholder="Your name" autoComplete="off" />
+          <input
+            type="text"
+            value={values.name}
+            name="name"
+            placeholder="Your name"
+            autoComplete="off"
+            onChange={handleChange}
+          />
         )}
-        <input type="email" placeholder="Your email" autoComplete="off" />
-        <input type="password" placeholder="Choose a secure password" />
+        <input
+          type="email"
+          value={values.email}
+          name="email"
+          placeholder="Your email"
+          autoComplete="off"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          value={values.password}
+          name="password"
+          placeholder="Choose a secure password"
+          onChange={handleChange}
+        />
         <div className="flex mt3">
           <button type="submit" className="button pointer mr2">
             Submit
           </button>
-          <button type="button" className="pointer button" onClick={() => setLogin(prevLogin => !prevLogin)}>
-            {login ? "need to create new account" :"already have an account?"}
+          <button
+            type="button"
+            className="pointer button"
+            onClick={() => setLogin((prevLogin) => !prevLogin)}
+          >
+            {login ? "need to create new account" : "already have an account?"}
           </button>
         </div>
       </form>

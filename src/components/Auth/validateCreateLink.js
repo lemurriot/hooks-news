8bbd997934eb183f7ee7348a3063e44f1b150cfc
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-06-14 19:33:50 -0700

    implement create method for new submissions, persist to firestore

--- src/components/Auth/validateCreateLink.js
+++ src/components/Auth/validateCreateLink.js
@@ -1,1 +1,22 @@
-export default function validateCreateLink(values) {}
+export default function validateCreateLink(values) {
+  const errors = {};
+  const urlRE = /^(ftp|http|https):\/\/[^ "]+$/;
+
+  // Description errors
+  if (!values.description) {
+    errors.description = 'Description required';
+  } else if (values.description.length < 10) {
+    errors.description = 'Description must be at least 10 characters';
+  } else if (values.description.length > 700) {
+    errors.description = 'Description must not exceed 700 characters';
+  }
+
+  // Url errors
+  if (!values.url) {
+    errors.url = 'URL required';
+  } else if (!urlRE.test(values.url)) {
+    errors.url = 'URL must be valid';
+  }
+
+  return errors;
+}
--- src/components/Link/CreateLink.js
+++ src/components/Link/CreateLink.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import useFormValidation from '../Auth/useFormValidation';
+import validateCreateLink from '../Auth/validateCreateLink';
+import FirebaseContext from '../../firebase/context';
+
+const INITIAL_STATE = {
+  description: '',
+  url: '',
+};
 
 function CreateLink(props) {
-  return <div>CreateLink</div>;
+  const { firebase, user } = useContext(FirebaseContext);
+  const { handleSubmit, handleChange, values, errors } = useFormValidation(INITIAL_STATE, validateCreateLink, handleCreateLink);
+  function handleCreateLink() {
+    if (!user) {
+      props.history.push('/login')
+    } else {
+      const { url, description } = values;
+      const newLink = {
+        url,
+        description,
+        postedBy: {
+          id: user.uid,
+          name: user.displayName
+        },
+        votes: [],
+        comments: [],
+        created: Date.now(),
+      }
+      firebase.db.collection('links').add(newLink);
+      props.history.push('/');
+    }
+  }
+  return (
+    <form className="flex flex-column" onSubmit={handleSubmit}>
+      <input
+        className={errors.description && 'error-input'}
+        name="description"
+        placeholder="A description for your link"
+        autoComplete="off"
+        type="text"
+        onChange={handleChange}
+      />
+      {errors.description && <p className="error-text">{errors.description}</p>}
+      <input
+        className={errors.url && 'error-input'}
+        name="url"
+        placeholder="The url for the link"
+        autoComplete="off"
+        type="url"
+        onChange={handleChange}
+      />
+      {errors.url && <p className="error-text">{errors.url}</p>}
+      <button className="button" type="submit">
+        Submit
+      </button>
+    </form>
+  );
 }
 
 export default CreateLink;
